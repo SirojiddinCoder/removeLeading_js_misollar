83be67d1306a58001fa615c7f7cf6dee0e1a426c
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2021-10-30 13:25:36 -0700

    qidir function

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
-//  Create a function that takes in a number as a string n and returns the number without trailing and leading zeros.
-//   Trailing Zeros are the zeros after a decimal point which don't affect the value (e.g. the last three zeros in 3.4000 and 3.04000).
-//   Leading Zeros are the zeros before a whole number which don't affect the value (e.g. the first three zeros in 000234 and 000230).
-
+// Write a function that takes an integer i and returns an integer with the integer backwards followed by the original integer.
+// To illustrate:
+// 123
+// We reverse 123 to get 321 and then add 123 to the end, resulting in 321123.
 
 1;
 // Create a function that takes in a number as a string n and returns the number without trailing and leading zeros.
@@ -51,7 +51,7 @@
 
 // insertWhitespace("TheGreatestUpsetInHistory") ➞ "The Greatest Upset In History"
 
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // 1-masala  removeLeadingTrailing("230.000") ➞ "230"
 
@@ -65,7 +65,7 @@
 
 //   console.log(str1(230.000));
 
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 2 -usul while da
 // let n = "230.000"
 // Result  = (parseFloat(n));
@@ -76,3 +76,49 @@
 // }
 
 // console.log(Result);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//   1- usul   reverseAndNot(123456789) ➞ 987654321123456789
+// const number = function(x){
+//     let a = 0, b = 0;
+//     while (x!==0) {
+//         b = x % 10;
+//         x = parseInt(x/10);
+//         a = a * 10 + b;
+//         if (a < Math.pow(-2, 31) || a > Math.pow(2, 31) - 1)
+//         return 0
+//     }
+//     return a
+// }
+
+// console.log(number(123456789));
+
+// 2 - usuli
+// let num = 123;
+// function reverseAndNot(num) {
+//     let rev_num = 0;
+//     while(num > 0)
+//     {
+//         rev_num = rev_num * 10 + num % 10;
+//         num = Math.floor(num / 10);
+//     }
+//     return rev_num;
+// }
+
+// console.log(reverseAndNot(num));;
+
+//  2- masala testJackpot(["@", "@", "@", "@"]) ➞ true
+function qidir(a, b, c, d) {
+    let Arr = [a, b, c, d];
+    for (let i = 1; i < Arr.length; i++) {
+        if (Arr[0] !== Arr[i])
+         return false;
+    }
+    return true;
+}
+
+
+let Result = qidir("SS", "SS", "tt", "SS");
+
+console.log(Result);
